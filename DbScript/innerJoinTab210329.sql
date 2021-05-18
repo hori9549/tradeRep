f78c643aaaff55eee1ede814b02fad75d31e74fb
hori9549 <hori9549@gmail.com>
2021-05-19 07:11:39 +0900

    tradeRep clone DbScript delete 210519

--- DbScript/innerJoinTab210329.sql
+++
@@ -1,8 +0,0 @@
-/****** SSMS の SelectTopNRows コマンドのスクリプト  ******/
-SELECT　a.入力ID ,a.銘柄コード　,a.銘柄名 ,a.取得日付
-,b.返済元ID ,b.返済日付
-  FROM [MyTradeDB].[dbo].[MST_取得] as a　
-  inner join
-  [MyTradeDB].[dbo].[MST_返済] as b	　on a.入力ID = b.返済元ID
- where  取得日付>='2021/1/1' and 取得日付<'2021/2/1'
-/*銘柄コード=9984*/
--- DbScript/日付から返済銘柄を検索.sql
+++
@@ -1,3 +0,0 @@
-select a.*,b.–Á•¿–¼ from MST_•ÔÏ as a inner join  MST_æ“¾ as b
-on a.•ÔÏŒ³ID=b.“ü—ÍID
-where •ÔÏ“ú•t='2021/3/8'
