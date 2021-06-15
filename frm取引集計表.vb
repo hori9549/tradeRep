ded03d264f4d87d7ed955629361d8b744ab3afc2
hori9549 <hori9549@gmail.com>
2021-06-15 17:15:14 +0900

    groupby

--- frm取引集計表.vb
+++ frm取引集計表.vb
@@ -15,10 +15,12 @@ Public Class frm取引集計表
         Dim mSDA As New SqlDataAdapter
         Dim dtbl検索結果 As New DataTable
 
-        msSQL = " Select  a.ID  as 取得ID, a.取得日付 , b.ID As 返済ID , b.返済日付 , a.銘柄コード As CODE , a.銘柄名 , a.取引株数 as 取得株数, b.返済株数 ,a.残株数 "
+        msSQL = " Select  a.ID  as 取得ID, a.取得日付 , b.ID As 返済ID , b.返済日付 "
+        msSQL += " , a.銘柄コード As CODE , a.銘柄名 , a.取引株数 as 取得株数, b.返済株数 ,a.残株数 "
+        msSQL += ", format(b.[返済株数] * b.[返済単価] - a.取引株数 * 取得単価,'#,##0') as 損益"
         msSQL += " From MST_取得 As A left Join MST_返済 As b On a.入力ID = b.返済元ID"
 
-        msSQL += " Where a.銘柄コード = '6857'"
+        msSQL += " Where a.銘柄コード = '8604'"
         mCommand = cDB.getsqlComand(msSQL)
         mSDA.SelectCommand = mCommand
 
