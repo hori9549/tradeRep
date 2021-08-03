5ee2acafc230f419024d3e0d22f32131edfbd78b
hori9549 <hori9549@gmail.com>
2021-08-03 16:30:33 +0900

    yakujyou

--- frmメニュー2.Designer.vb
+++ frmメニュー2.Designer.vb
@@ -1,7 +1,7 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class frmメニュー2
-    Inherits System.Windows.Forms.Form
-
+    ' Inherits System.Windows.Forms.Form
+    Inherits frmベース
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
@@ -28,16 +28,25 @@ Partial Class frmメニュー2
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.lblVersion = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btn約定通知 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
         CType(Me.pcbTop, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.grp導入.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'pcbTop
         '
         Me.pcbTop.Image = Global.カシオペイア.My.Resources.Resources.虫眼鏡イラストS
-        Me.pcbTop.Location = New System.Drawing.Point(24, 75)
+        Me.pcbTop.Location = New System.Drawing.Point(40, 119)
+        Me.pcbTop.Margin = New System.Windows.Forms.Padding(5)
         Me.pcbTop.Name = "pcbTop"
-        Me.pcbTop.Size = New System.Drawing.Size(249, 193)
+        Me.pcbTop.Size = New System.Drawing.Size(259, 192)
         Me.pcbTop.TabIndex = 0
         Me.pcbTop.TabStop = False
         '
@@ -47,9 +56,11 @@ Partial Class frmメニュー2
         Me.grp導入.Controls.Add(Me.Button2)
         Me.grp導入.Controls.Add(Me.Button1)
         Me.grp導入.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.grp導入.Location = New System.Drawing.Point(24, 289)
+        Me.grp導入.Location = New System.Drawing.Point(41, 350)
+        Me.grp導入.Margin = New System.Windows.Forms.Padding(5)
         Me.grp導入.Name = "grp導入"
-        Me.grp導入.Size = New System.Drawing.Size(249, 132)
+        Me.grp導入.Padding = New System.Windows.Forms.Padding(5)
+        Me.grp導入.Size = New System.Drawing.Size(333, 180)
         Me.grp導入.TabIndex = 1
         Me.grp導入.TabStop = False
         Me.grp導入.Text = "導入処理"
@@ -57,9 +68,10 @@ Partial Class frmメニュー2
         'Button2
         '
         Me.Button2.BackColor = System.Drawing.Color.Cyan
-        Me.Button2.Location = New System.Drawing.Point(30, 69)
+        Me.Button2.Location = New System.Drawing.Point(22, 109)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(5)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(170, 36)
+        Me.Button2.Size = New System.Drawing.Size(283, 57)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "Button2"
         Me.Button2.UseVisualStyleBackColor = False
@@ -67,9 +79,10 @@ Partial Class frmメニュー2
         'Button1
         '
         Me.Button1.BackColor = System.Drawing.Color.Cyan
-        Me.Button1.Location = New System.Drawing.Point(30, 18)
+        Me.Button1.Location = New System.Drawing.Point(22, 29)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(170, 36)
+        Me.Button1.Size = New System.Drawing.Size(283, 57)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = False
@@ -78,7 +91,8 @@ Partial Class frmメニュー2
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.Label1.Location = New System.Drawing.Point(22, 9)
+        Me.Label1.Location = New System.Drawing.Point(37, 14)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(132, 19)
         Me.Label1.TabIndex = 2
@@ -88,24 +102,111 @@ Partial Class frmメニュー2
         '
         Me.lblVersion.AutoSize = True
         Me.lblVersion.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.lblVersion.Location = New System.Drawing.Point(22, 37)
+        Me.lblVersion.Location = New System.Drawing.Point(37, 59)
+        Me.lblVersion.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.lblVersion.Name = "lblVersion"
         Me.lblVersion.Size = New System.Drawing.Size(0, 19)
         Me.lblVersion.TabIndex = 3
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.btn約定通知)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Blue
+        Me.GroupBox1.Location = New System.Drawing.Point(402, 350)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(5)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(5)
+        Me.GroupBox1.Size = New System.Drawing.Size(262, 180)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "取引登録"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Lime
+        Me.Button3.Location = New System.Drawing.Point(10, 109)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(5)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(236, 57)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'btn約定通知
+        '
+        Me.btn約定通知.BackColor = System.Drawing.Color.Lime
+        Me.btn約定通知.Location = New System.Drawing.Point(10, 29)
+        Me.btn約定通知.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn約定通知.Name = "btn約定通知"
+        Me.btn約定通知.Size = New System.Drawing.Size(236, 57)
+        Me.btn約定通知.TabIndex = 0
+        Me.btn約定通知.Text = "Gmail約定通知から"
+        Me.btn約定通知.UseVisualStyleBackColor = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GroupBox2.Controls.Add(Me.Button5)
+        Me.GroupBox2.Controls.Add(Me.Button6)
+        Me.GroupBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.GroupBox2.Location = New System.Drawing.Point(690, 350)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(5)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(5)
+        Me.GroupBox2.Size = New System.Drawing.Size(348, 180)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "導入処理"
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Cyan
+        Me.Button5.Location = New System.Drawing.Point(50, 109)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(5)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(283, 57)
+        Me.Button5.TabIndex = 1
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Cyan
+        Me.Button6.Location = New System.Drawing.Point(50, 29)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(5)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(283, 57)
+        Me.Button6.TabIndex = 0
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
         'frmメニュー2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 19.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1184, 761)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.lblVersion)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.grp導入)
         Me.Controls.Add(Me.pcbTop)
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(7, 8, 7, 8)
         Me.Name = "frmメニュー2"
         Me.Text = "frmメニュー2"
+        Me.Controls.SetChildIndex(Me.pcbTop, 0)
+        Me.Controls.SetChildIndex(Me.grp導入, 0)
+        Me.Controls.SetChildIndex(Me.Label1, 0)
+        Me.Controls.SetChildIndex(Me.lblVersion, 0)
+        Me.Controls.SetChildIndex(Me.GroupBox1, 0)
+        Me.Controls.SetChildIndex(Me.GroupBox2, 0)
         CType(Me.pcbTop, System.ComponentModel.ISupportInitialize).EndInit()
         Me.grp導入.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -117,4 +218,10 @@ Partial Class frmメニュー2
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents lblVersion As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents btn約定通知 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
 End Class
--- frmメニュー2.vb
+++ frmメニュー2.vb
@@ -1,4 +1,10 @@
-﻿Public Class frmメニュー2
+﻿Imports System.Data.SqlClient
+
+Public Class frmメニュー2
+    '  Private msSQL As String
+    Private mCommand As SqlCommand
+    Private mSDA As New SqlDataAdapter
+
     Private Sub frmメニュー2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim sfrm As New sfrmログイン
         Select Case sfrm.ShowDialog()
@@ -18,4 +24,304 @@
         my環境設定 = New cls環境設定
     End Sub
 
+    Private Sub btn約定通知_Click(sender As Object, e As EventArgs) Handles btn約定通知.Click
+        Dim cDB As New clsDB
+        Dim msSQL As String
+        Dim myTable As New DataTable
+        Dim g取引名称 As String    　 'Excelに記されたgmailのデーター
+        Dim g銘柄コード As String     'Excelに記されたgmailのデーター
+        Dim g銘柄名 As String         'Excelに記されたgmailのデーター
+        Dim g株数 As String           'Excelに記されたgmailのデーター
+        Dim g価格 As String           'Excelに記されたgmailのデーター
+        Dim g日付 As String           'Excelに記されたgmailのデーター
+        ' Dim sFname As String = "C:\Users\hori9\OneDrive\ドキュメント\Gmail約定通知2021625_629.xlsx"
+
+        'Excelファイルを開く
+        Dim SFD As New OpenFileDialog
+        Dim sFname As String        'selected file name 
+        'Dim di As New System.IO.DirectoryInfo(sフォルダ)
+        'di.Create()
+        With SFD
+            .Filter = "excelファイル(*.xlsx,*.xlmx)|*.xlsx;*.xlmx"
+            .Title = "excelファイルを選択してください"
+            .RestoreDirectory = True
+            .CheckFileExists = True
+            .CheckPathExists = True
+
+            '' dialog　ＯＫ が押されたら次へ
+            If .ShowDialog <> Windows.Forms.DialogResult.OK Then Exit Sub
+
+            MsgBox(.FileName)
+            Console.WriteLine(.FileName)
+            sFname = .FileName
+        End With
+
+
+
+
+
+        Using workbook = New ClosedXML.Excel.XLWorkbook(sFname)
+
+            ''/****************ワークシートを取得する************************/
+            Dim worksheet As ClosedXML.Excel.IXLWorksheet = workbook.Worksheet("約定通知")
+            '  Dim worksheet As ClosedXML.Excel.IXLWorksheet = workbook.Worksheet("retry")
+
+            ''/*****************空白がでてくるまで読み続ける******************/
+            Dim i As Integer = 1
+            Do While (worksheet.Cell(i, "A").Value) <> ""   '空白がでてくるまで読み続ける
+
+                ''/**** 共通部分 Excel Gmail データーを取得 ****/
+                Dim dt As String
+
+                dt = (worksheet.Cell(i, "G").Value)   '取引名称 & vbLF  
+                g取引名称 = dt.Substring(0, (dt.Length - 1))
+
+                g銘柄コード = worksheet.Cell(i, "I").Value
+
+                dt = worksheet.Cell(i, "J").Value       '銘柄名 &vbLF
+                g銘柄名 = dt.Substring(0, (dt.Length - 1))
+
+                dt = worksheet.Cell(i, "L").Value        '"株数:1,000 &vbLF
+                dt = dt.Substring(3)
+                dt = dt.Substring(0, dt.Length - 1)
+                g株数 = dt.Replace(",", "") '","をのぞく
+
+                dt = (worksheet.Cell(i, "m").Value).substring(3)    '価格:#,###&vbLF
+                dt = dt.Substring(0, dt.Length - 1)
+                g価格 = dt.Replace(",", "") '","をのぞく
+
+                dt = worksheet.Cell(i, "d").Value        ' g日付
+                g日付 = dt.Substring(0, 10)
+
+
+
+                '/************** 取引種別ごとに分ける *****************/
+                Select Case g取引名称
+                    Case "信用新規買"
+                        '  If g取引名称 = "信用新規買" Then
+
+                        ''信用新規買 登録
+                        '  txt入力ID.Text = 入力ID最大取得.maxID
+                        Dim 入力ID As String = getMaxId().ToString
+
+                        '         Select Case MessageBox.Show("" & g銘柄コード & " " & g銘柄名 & "を" _
+                        ' & g株数 & " " & g価格 & "'新規買'で、登録しますか？",
+                        '"確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+                        '             Case Windows.Forms.DialogResult.Yes
+                        '             Case Else
+                        '                 GoTo nextRec
+                        '         End Select
+                        If System.Windows.Forms.DialogResult.Yes <>
+        MessageBox.Show("" & g銘柄コード & " " & g銘柄名 & "を" _
+& g株数 & " " & g価格 & "'新規買'で、登録しますか？",
+"確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) Then
+
+                            'Case Windows.Forms.DialogResult.Yes
+                            'Case Else
+                            '    GoTo nextRec
+                            worksheet.Cell(i, "N").Value = "NG"
+                            GoTo nextRec
+                        End If
+                        '追加Dataのセット
+                        msSQL = " INSERT INTO MTD_取得 ( "
+                        msSQL += " [入力ID]"
+                        msSQL += " ,[銘柄コード]"
+                        msSQL += " ,[銘柄名]"
+                        msSQL += " ,[取引名称]"
+                        msSQL += " ,[取得株数]"
+                        msSQL += " ,[取得単価]"
+                        msSQL += " ,[取得日付]"
+                        msSQL += " ,[残株数]"
+                        msSQL += " ,[現況]"
+                        msSQL += " )"
+
+                        msSQL += "  VALUES "
+                        msSQL += " ('" & 入力ID & "'"                        ' <入力ID, nvarchar(10),>
+                        msSQL += ",'" & g銘柄コード & "'"
+                        msSQL += ",'" & g銘柄名 & "'"
+                        msSQL += ",'信用新規買'"
+                        msSQL += ",'" & g株数 & "'"
+                        msSQL += ",'" & g価格 & "'"
+                        msSQL += ",'" & g日付 & "'"   ' g日付
+                        msSQL += ",'" & g株数 & "'"
+                        msSQL += ",'買建'"
+                        msSQL += ")"
+
+                        Try
+
+                            mCommand = cDB.getsqlComand(msSQL)
+                            Call mCommand.ExecuteNonQuery()
+                            '  btn続けて入力.Select()
+
+                        Catch ex As Exception
+                            '   OorN = False      '失敗
+                            MsgBox("新規登録は、失敗！")
+                            btn閉じる.Select()
+                        End Try
+
+                    ' 
+                    ''信用返済売 
+                    '------------------------------
+                    Case "信用返済売", "現物売"
+                        Dim 入力ID As String = getMaxId().ToString
+
+                        '  g銘柄コード = worksheet.Cell(i, "I").Value
+
+                        'dt = worksheet.Cell(i, "J").Value       '銘柄名 &vbLF
+                        'g銘柄名 = dt.Substring(0, (dt.Length - 1))
+
+                        'dt = worksheet.Cell(i, "L").Value        '"株数:1,000 &vbLF
+                        'dt = dt.Substring(3)
+                        'dt = dt.Substring(0, dt.Length - 1)
+                        'g株数 = dt.Replace(",", "") '","をのぞく
+
+                        'dt = (worksheet.Cell(i, "m").Value).substring(3)    '価格:#,###&vbLF
+                        'dt = dt.Substring(0, dt.Length - 1)
+                        'g価格 = dt.Replace(",", "") '","をのぞく
+
+                        'dt = worksheet.Cell(i, "d").Value        ' g日付
+                        'g日付 = dt.Substring(0, 10)
+
+
+                        Dim sfrm As New sfrmExcel返済
+                        sfrm.txtg銘柄コード.Text = g銘柄コード
+                        sfrm.txtg銘柄名.Text = g銘柄名
+                        sfrm.txtg返済株数.Text = g株数
+                        sfrm.txtg価格.Text = g価格
+                        sfrm.txtg日付.Text = g日付
+                        sfrm.txtg取引名称.Text = g取引名称
+                        If System.Windows.Forms.DialogResult.OK <> sfrm.ShowDialog() Then   'sfrmExcel返済に一任する
+                            worksheet.Cell(i, "N").Value = "NG"
+                        End If
+                         ' 
+                    ''　現物買 
+                    '------------------------------
+                    Case "現物買"
+                        Dim 入力ID As String = getMaxId().ToString
+
+                        'g銘柄コード = worksheet.Cell(i, "I").Value
+
+                        'dt = worksheet.Cell(i, "J").Value       '銘柄名 &vbLF
+                        'g銘柄名 = dt.Substring(0, (dt.Length - 1))
+
+
+                        'dt = worksheet.Cell(i, "L").Value        '"株数:1,000 &vbLF
+                        'dt = dt.Substring(3)
+                        'dt = dt.Substring(0, dt.Length - 1)
+                        'g株数 = dt.Replace(",", "") '","をのぞく
+
+                        'dt = (worksheet.Cell(i, "m").Value).substring(3)    '価格:#,###&vbLF
+                        'dt = dt.Substring(0, dt.Length - 1)
+                        'g価格 = dt.Replace(",", "") '","をのぞく
+
+                        'dt = worksheet.Cell(i, "d").Value        ' g日付
+                        'g日付 = dt.Substring(0, 10)
+
+
+                        If System.Windows.Forms.DialogResult.Yes <>
+                         MessageBox.Show("" & g銘柄コード & " " & g銘柄名 & "を" _
+                & g株数 & " " & g価格 & "'現物買'で、登録しますか？",
+               "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) Then
+
+                            'Case Windows.Forms.DialogResult.Yes
+                            'Case Else
+                            '    GoTo nextRec
+                            worksheet.Cell(i, "N").Value = "NG"
+                            GoTo nextRec
+
+                        End If
+
+
+
+                        '追加Dataのセット
+                        msSQL = " INSERT INTO MTD_取得 ( "
+                        msSQL += " [入力ID]"
+                        msSQL += " ,[銘柄コード]"
+                        msSQL += " ,[銘柄名]"
+                        msSQL += " ,[取引名称]"
+                        msSQL += " ,[取得株数]"
+                        msSQL += " ,[取得単価]"
+                        msSQL += " ,[取得日付]"
+                        msSQL += " ,[残株数]"
+                        msSQL += " ,[現況]"
+                        msSQL += " )"
+
+                        msSQL += "  VALUES "
+                        msSQL += " ('" & 入力ID & "'"                        ' <入力ID, nvarchar(10),>
+                        msSQL += ",'" & g銘柄コード & "'"
+                        msSQL += ",'" & g銘柄名 & "'"
+                        msSQL += ",'現物買'"
+                        msSQL += ",'" & g株数 & "'"
+                        msSQL += ",'" & g価格 & "'"
+                        msSQL += ",'" & g日付 & "'"   ' g日付
+                        msSQL += ",'" & g株数 & "'"
+                        msSQL += ",'特定預り'"
+                        msSQL += ")"
+
+                        Try
+
+                            mCommand = cDB.getsqlComand(msSQL)
+                            Call mCommand.ExecuteNonQuery()
+                            '  btn続けて入力.Select()
+
+                        Catch ex As Exception
+                            '   OorN = False      '失敗
+                            MsgBox("新規登録は、失敗！")
+                            btn閉じる.Select()
+                        End Try
+
+                    ' 
+                    ''　現引 
+                    '------------------------------
+                    Case "現引"
+                        '  MsgBox($"{g取引名称}は、未完成です")
+
+                        'Dim 入力ID As String = getMaxId().ToString
+
+                        ' g銘柄コード = worksheet.Cell(i, "I").Value
+
+                        'dt = worksheet.Cell(i, "J").Value       '銘柄名 &vbLF
+                        ' g銘柄名 = dt.Substring(0, (dt.Length - 1))
+
+                        'dt = worksheet.Cell(i, "L").Value        '"株数:1,000 &vbLF
+                        'dt = dt.Substring(3)
+                        'dt = dt.Substring(0, dt.Length - 1)
+                        ' g株数 = dt.Replace(",", "") '","をのぞく
+
+                        'dt = (worksheet.Cell(i, "m").Value).substring(3)    '価格:#,###&vbLF
+                        'dt = dt.Substring(0, dt.Length - 1)
+                        ' g価格 = dt.Replace(",", "") '","をのぞく
+
+                        'dt = worksheet.Cell(i, "d").Value        ' g日付
+                        ' g日付 = dt.Substring(0, 10)
+
+                        Dim sfrm As New sfrmExcel返済
+                        sfrm.txtg銘柄コード.Text = g銘柄コード
+                        sfrm.txtg銘柄名.Text = g銘柄名
+                        sfrm.txtg返済株数.Text = g株数
+                        sfrm.txtg価格.Text = g価格
+                        sfrm.txtg日付.Text = g日付
+                        sfrm.txtg取引名称.Text = g取引名称
+                        If System.Windows.Forms.DialogResult.OK <> sfrm.ShowDialog() Then   'sfrmExcel返済に一任する
+                            worksheet.Cell(i, "N").Value = "NG"
+                        End If
+
+                    Case Else
+                        MsgBox($"{g取引名称}は、未完成です")
+                        worksheet.Cell(i, "N").Value = "NG"
+
+                End Select
+nextRec:        i += 1
+            Loop
+            ' // ワークブックを保存する
+            workbook.SaveAs(sFname)
+
+            MsgBox("全Recを手続きしました")
+
+        End Using
+
+
+
+    End Sub
 End Class
